c036e6b4208350e4602f6fe73a4724592bcc5f30
Bet Mendoza <sbm2391@hotmail.com>
2018-01-15 16:49:19 -0600

    subir y bajar

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,37 +1,57 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
+  constructor(floor, direction){
+    this.floor      = floor;
     this.MAXFLOOR   = 10;
+    this.MINFLOOR   = 0;
     this.requests   = [];
-    this.direction  = "up"
+    this.direction  = direction;
+
   }
 
   start() {
     let that = this;
-    setInterval(function(){ that.update() }, 1000);
-
-  }
-  stop() {
-    let interval = this.start();
-
+    this.interval = setInterval(function(){ 
+      that.update() }, 1000);
     
-    clearTimeout(interval);
+    }
+  
+
+  stop() {
+   
+    clearInterval(this.interval)
+  
   }
   update() {
-    return this.log();
+    if (this.direction === "up") {
+      if (this.floor < this.MAXFLOOR) {
+        this.floorUp();
+        return this.log();
+      } else {
+        this.stop();
+      }
+    } else if (this.direction === "down") {
+      if (this.floor >  this.MINFLOOR) {
+        this.floorDown ();
+        return this.log();
+      } else {
+        this.stop();
+      }
+    }
+    
   }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() {
-    return this.floor++;
+    this.floor++;
   }
+
   floorDown() {
-    return this.floor--;
+    this.floor--;
   }
+
   call() { }
-  log() {
-    
-    console.log( `Direction: ${this.direction} | Floor: ${this.floorUp()} `);
+  log(){
+    console.log( `Direction: ${this.direction} | Floor: ${this.floor} `);
   }
 }
 
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,5 +1,6 @@
 const Elevator = require('./elevator.js');
 const Person = require('./person.js');
 
-const elevator1 = new Elevator();
+const elevator1 = new Elevator(5, "down");
+
 elevator1.start();
